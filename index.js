febdc79e7b09190fd1256bce38eaeab968a1404c
kljohn71 <135288852+kljohn71@users.noreply.github.com>
2023-11-09 14:48:35 -0700

    revert index.js to original code

--- index.js
+++ index.js
@@ -2,23 +2,8 @@
  * @format
  */
 
-import { CometChat } from "@cometchat-pro/react-native-chat";
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 AppRegistry.registerComponent(appName, () => App);
-const appID = "245207bd98c0e574";
-const region = "US";
-let appSetting = new CometChat.AppSettingsBuilder()
-                        .subscribePresenceForAllUsers()
-                        .setRegion(region)
-                        .autoEstablishSocketConnection(true)
-                        .build();
-CometChat.init(appID, appSetting).then(
-    () => {
-        console.log("Initialization completed successfully");
-    }, error => {
-        console.log("Initialization failed with error:", error);
-    }
-);
